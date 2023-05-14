a5ff40a9a654c6d63f752a14425cadf56a66c088
Andrii-Pershko <andriiinf@gmail.com>
2023-05-14 14:39:14 +0300

    add logout

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,22 @@
+import { Route, Routes, useNavigate } from 'react-router-dom';
+import { SignUp } from '../pages/SingUp/SignUp';
+import { SharedLayout } from './SharedLayout/SharedLayout';
+import { Home } from '../pages/Home/Home';
+import { Login } from 'pages/Login/Login';
+
 export const App = () => {
+  // const navigate = useNavigate();
+  // useEffect(() => {
+  //   navigate('/');
+  // }, []);
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <Routes>
+      <Route path="/" element={<SharedLayout />}>
+        <Route index element={<Home />} />
+        <Route path="/registration" element={<SignUp />}></Route>
+        <Route path="/login" element={<Login />}></Route>
+      </Route>
+    </Routes>
   );
 };
